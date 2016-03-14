f782333fa798f725d8bc29d379d615bfc3c121e7
Mohamed Chalal <Mohamedrchalal@gmail.com>
2016-03-14 15:43:10 -0400

    checkboxes work

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -3,8 +3,12 @@ class FormsController < ApplicationController
   def post
     # handle post requests here
   end
-
+  
   def index
+    if params[:q]
+    @languages = Language.where(name: params[:q])
+    else
     @languages = Language.all
+    end
   end
 end
